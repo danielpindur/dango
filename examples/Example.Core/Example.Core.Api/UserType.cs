60a10cf692c5d5e3696d9869063bf7ae8f3f8391
Daniel Pindur <pindurdan@gmail.com>
2025-10-23 17:44:08 +0200

    chore: add demo example dotnet CORE project

---
+++ examples/Example.Core/Example.Core.Api/UserType.cs
@@ -0,0 +1,7 @@
+﻿namespace Example.Core.Api;
+
+public enum UserType
+{
+    NormalUser,
+    AdminUser
+}
---
+++ examples/Example.Core/Example.Core/MappingRegistrar.cs
@@ -0,0 +1,12 @@
+﻿using Dango.Abstractions;
+using ApiUserType = Example.Core.Api.UserType;
+
+namespace Example.Core;
+
+public class MappingRegistrar : IDangoMapperRegistrar
+{
+    public void Register(IDangoMapperRegistry registry)
+    {
+        registry.Enum<ApiUserType, UserType>();
+    }
+}
